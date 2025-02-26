f43f2591e3beb6ecdf2f09fc62ff50b7ec506aa7
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-02-26 13:26:34 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ["raleway", "serif"],
+        Roboto: ["Roboto", "serif"],
+      },
+      animation: {
+        bounceSlow: " bounce 15s infinite",
+      },
+      textShadow: {
+        lg: "2px 2px 4px rgba(0,0,0, 0.5)",
+      },
+    },
+  },
+  plugins: [],
+};
